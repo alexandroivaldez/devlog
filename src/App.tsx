ee6196099afe9884ef2782faca0f967dc89244b3
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-12-25 14:43:57 -0600

    Updated bucket_id

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Testing GitHub Action</h1>
+      <h1>Testing GitHub Action v2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
